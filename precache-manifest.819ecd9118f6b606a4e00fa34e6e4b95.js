6ce331d8d2817e7bf0e2de2d440352c95c535a1a
BenBezac <benjamin.bezancon@gmail.com>
2020-04-10 14:59:27 +0200

    Updates

---
+++ precache-manifest.819ecd9118f6b606a4e00fa34e6e4b95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce66eb127aed0cbf3a180f3728a1a23a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cc7bac746836cd82c713",
+    "url": "/monsters-rolodex/static/css/main.93cdc26e.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc7bac746836cd82c713",
+    "url": "/monsters-rolodex/static/js/main.64319c87.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
